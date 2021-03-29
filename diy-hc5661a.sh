7b38a064d0bbfef4667976416590da06cb5aadb9
thofd <30772676+thofd@users.noreply.github.com>
2021-03-29 12:25:08 +0800

    Update diy-hc5661a.sh
    1.change hostname is hc5661a
    2.change pw is jzz123456

--- diy-hc5661a.sh
+++ diy-hc5661a.sh
@@ -5,6 +5,17 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
+# 修改默认IP
 sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 sed -i 's/root:$1$ZxXjr/8S$gS4ClQTeJuNssAsXuJoQ.0:18435:0:99999:7:::/g' /etc/shadow
+#2.修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='HC5661A'' package/lean/default-setting/files/zzz-default-setting
+#3.修改默认密码为空
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-setting/files/zzz-default-setting
+#4.版本号里显示一个自己的名字
+#sed -i "s/OpenWrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-setting/files/zzz-default-setting
+#5.修改插件名字
+#sed -i 's/"Turbo Acc 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/po/zh-ch/sfe.po
+#6.修改上游DNS
+#sed -i "2i # network config" package/lean/default-setting/files/zzz-default-setting
+#sed -i "3i uci set network.lan.dns='10.10.10.2'" package/lean/default-setting/files/zzz-default-setting
