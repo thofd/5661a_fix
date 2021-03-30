806f3410b08e2f208fcafd5277ec5dc01a1cdc68
thofd <30772676+thofd@users.noreply.github.com>
2021-03-30 15:11:06 +0800

    Update diy-hc5661a.sh

--- diy-hc5661a.sh
+++ diy-hc5661a.sh
@@ -5,19 +5,15 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
+#设置files目录权限
+chmod -R 755 files
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 #2.修改默认主机名
 sed -i 's/OpenWrt/HC5661A/g' package/base-files/files/bin/config_generate
 #sed -i 's/root:$1$ZxXjr/8S$gS4ClQTeJuNssAsXuJoQ.0:18435:0:99999:7:::/g' /etc/shadow
-#sed -i '/uci commit system/i\uci set system.@system[0].hostname='HC5661A'' package/base-files/files/bin/config_generate
-#sed -i 's/root:$1$ZxXjr/8S$gS4ClQTeJuNssAsXuJoQ.0:18435:0:99999:7:::/g' /etc/shadow
 #3.修改默认密码
-#sed -i 's/root::0:0:99999:7:::/root:$1$WWdfWplo$F3cJ4znR/4yRSrgNtHk8m1:0:0:99999:7:::/g' package/base-files/files/etc/shadow
-#=================================================
-# 设置默认密码为空
-# #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-setting/files/zzz-default-setting
-#=================================================
+sed -i 's/root::0:0:99999:7:::/root:$1$WWdfWplo$F3cJ4znR/4yRSrgNtHk8m1:0:0:99999:7:::/g' /etc/shadow
 #4.版本号里显示一个自己的名字
 #sed -i "s/OpenWrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-setting/files/zzz-default-setting
 #5.修改插件名字
