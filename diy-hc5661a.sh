8ed673f732c50fd9bc607a860dbeca072eedccbd
thofd <30772676+thofd@users.noreply.github.com>
2021-03-30 15:32:35 +0800

    Update diy-hc5661a.sh

--- diy-hc5661a.sh
+++ diy-hc5661a.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_gener
 sed -i 's/OpenWrt/HC5661A/g' package/base-files/files/bin/config_generate
 #sed -i 's/root:$1$ZxXjr/8S$gS4ClQTeJuNssAsXuJoQ.0:18435:0:99999:7:::/g' /etc/shadow
 #3.修改默认密码
-sed -i '!root::0:0:99999:7:::!root:$1$WWdfWplo$F3cJ4znR/4yRSrgNtHk8m1:0:0:99999:7:::!g' /etc/shadow
+sed -i '!root::0:0:99999:7:::!root:$1$WWdfWplo$F3cJ4znR/4yRSrgNtHk8m1:0:0:99999:7:::!g' package/base-files/files/etc/shadow
 #4.版本号里显示一个自己的名字
 #sed -i "s/OpenWrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-setting/files/zzz-default-setting
 #5.修改插件名字
